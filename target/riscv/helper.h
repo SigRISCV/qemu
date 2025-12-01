c4235da722544b9e9bb0a455642ba55781f5fc7d
Zephyr <3230103990@zju.edu.cn>
2025-12-01 21:12:55 +0800

    add helper for ls, ss

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -128,6 +128,12 @@ DEF_HELPER_4(csrrw, tl, env, int, tl, tl)
 DEF_HELPER_2(csrr_i128, tl, env, int)
 DEF_HELPER_4(csrw_i128, void, env, int, tl, tl)
 DEF_HELPER_6(csrrw_i128, tl, env, int, tl, tl, tl, tl)
+#ifdef TARGET_SIGRISCV
+DEF_HELPER_2(sigriscv_get_gpr_id, tl, env, i32)
+DEF_HELPER_3(sigriscv_set_gpr_id, void, env, i32, tl)
+DEF_HELPER_3(sigriscv_encrypt_ptr, tl, env, tl, tl)
+DEF_HELPER_3(sigriscv_decrypt_ptr, tl, env, tl, tl)
+#endif
 #ifndef CONFIG_USER_ONLY
 DEF_HELPER_1(sret, tl, env)
 DEF_HELPER_1(mret, tl, env)
---
+++ target/riscv/op_helper_sigriscv.c
@@ -0,0 +1,59 @@
+#include "qemu/osdep.h"
+
+#ifdef TARGET_SIGRISCV
+
+#include "cpu.h"
+#include "exec/helper-proto.h"
+#include "target/riscv/cpu_bits.h"
+#include "qarma.h"
+
+static void get_sigriscv_key(CPURISCVState *env, target_ulong *kl, target_ulong *kh)
+{
+    *kl = env->skey[0];
+    *kh = env->skey[1];
+}
+
+target_ulong helper_sigriscv_encrypt_ptr(CPUArchState *env,
+                                         target_ulong plaintext,
+                                         target_ulong tweak)
+{
+    CPURISCVState *riscv_env = (CPURISCVState *)env;
+    target_ulong keyl, keyh;
+    get_sigriscv_key(riscv_env, &keyl, &keyh);
+    return qarma64_enc(plaintext, tweak, keyl, keyh, 7);
+}
+
+target_ulong helper_sigriscv_decrypt_ptr(CPUArchState *env,
+                                         target_ulong secret,
+                                         target_ulong tweak)
+{
+    CPURISCVState *riscv_env = (CPURISCVState *)env;
+    target_ulong keyl, keyh;
+    get_sigriscv_key(riscv_env, &keyl, &keyh);
+    return qarma64_dec(secret, tweak, keyl, keyh, 7);
+}
+
+target_ulong helper_sigriscv_get_gpr_id(CPUArchState *env, uint32_t reg)
+{
+    CPURISCVState *riscv_env = (CPURISCVState *)env;
+    
+    if (reg >= SIGCSR_GPRID_NUM) {
+        return 0;
+    }
+
+    return riscv_env->gpr_id[reg] & SIGCSR_ID_MASK;
+}
+
+void helper_sigriscv_set_gpr_id(CPUArchState *env, uint32_t reg, target_ulong id)
+{
+    CPURISCVState *riscv_env = (CPURISCVState *)env;
+    
+    if (reg == 0 || reg >= SIGCSR_GPRID_NUM) {
+        return;
+    }
+
+    riscv_env->gpr_id[reg] = id & SIGCSR_ID_MASK;
+}
+
+#endif /* TARGET_SIGRISCV */
+
