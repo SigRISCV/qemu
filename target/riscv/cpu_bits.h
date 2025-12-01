77c84b90ca4bb0518b06d18be56b156cbe7241e5
Zephyr <3230103990@zju.edu.cn>
2025-12-01 21:02:09 +0800

    add some macros

--- target/riscv/cpu_bits.h
+++ target/riscv/cpu_bits.h
@@ -627,6 +627,9 @@
 
 #define SIGCSR_GPRID_NUM    32
 #define SIGCSR_ID_MASK      0x00ffffffu
+#define SIGCSR_ID_SHIFT     40
+#define SIGCSR_PTR_BITS     40
+#define SIGCSR_PTR_MASK     ((1ULL << SIGCSR_PTR_BITS) - 1)
 #endif /* TARGET_SIGRISCV */
 
 /* mstatus CSR bits */
